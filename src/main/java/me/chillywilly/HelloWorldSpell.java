4e6ff5e762b99f2f03ed3e0a4658bcdadb961f0b
Chilllyy <117584956+Chilllyy@users.noreply.github.com>
2025-01-15 21:03:36 -0700

    fixed file layout

---
+++ src/main/java/me/chillywilly/HelloWorldSpell.java
@@ -0,0 +1,36 @@
+package me.chillywilly;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+
+import com.nisovin.magicspells.MagicSpells;
+import com.nisovin.magicspells.spells.InstantSpell;
+import com.nisovin.magicspells.util.CastResult;
+import com.nisovin.magicspells.util.MagicConfig;
+import com.nisovin.magicspells.util.SpellData;
+
+import net.kyori.adventure.audience.Audience;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.minimessage.MiniMessage;
+
+public class HelloWorldSpell extends InstantSpell {
+
+    public HelloWorldSpell(MagicConfig config, String spellName) {
+        super(config, spellName);
+    }
+
+    @Override
+    public CastResult cast(SpellData data) {
+        if (!(data.caster() instanceof Player)) return new CastResult(PostCastAction.ALREADY_HANDLED, data);
+
+        String msg = "<rainbow>Hello World!</rainbow>";
+        Component text = MiniMessage.miniMessage().deserialize(msg);
+        Bukkit.getOnlinePlayers().forEach((player) -> {
+            Audience aud = (Audience) player;
+            aud.sendMessage(text);
+        });
+        playSpellEffects(data);
+        MagicSpells.plugin.getServer().getPluginManager().disablePlugin(MagicSpells.plugin);
+        return new CastResult(PostCastAction.ALREADY_HANDLED, data);
+    }
+}
