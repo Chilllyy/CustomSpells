a351ab7d7136f879e7957954ef657e4f47a91416
Chilllyy <117584956+Chilllyy@users.noreply.github.com>
2025-01-15 19:32:13 -0700

    setup plugin files

---
+++ src/main/java/me/chillywilly/ChillySpells.java
@@ -0,0 +1,13 @@
+package me.chillywilly;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class ChillySpells extends JavaPlugin {
+    public static ChillySpells plugin;
+
+    @Override
+    public void onEnable() {
+        
+    }
+
+}
