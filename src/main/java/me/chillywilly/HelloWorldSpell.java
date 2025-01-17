b2c91018b28c7133e2816d4426efc1fa8d973795
Chilllyy <117584956+Chilllyy@users.noreply.github.com>
2025-01-16 19:31:21 -0700

    removed bloat code and shading

--- src/main/java/me/chillywilly/HelloWorldSpell.java
+++ src/main/java/me/chillywilly/HelloWorldSpell.java
@@ -1,17 +1,11 @@
 package me.chillywilly;
 
-import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
-import com.nisovin.magicspells.MagicSpells;
 import com.nisovin.magicspells.spells.InstantSpell;
 import com.nisovin.magicspells.util.CastResult;
 import com.nisovin.magicspells.util.MagicConfig;
 import com.nisovin.magicspells.util.SpellData;
-
-import net.kyori.adventure.audience.Audience;
-import net.kyori.adventure.text.Component;
-import net.kyori.adventure.text.minimessage.MiniMessage;
 
 public class HelloWorldSpell extends InstantSpell {
 
@@ -21,14 +15,11 @@ public class HelloWorldSpell extends InstantSpell {
 
     @Override
     public CastResult cast(SpellData data) {
-        if (!(data.caster() instanceof Player)) return new CastResult(PostCastAction.ALREADY_HANDLED, data);
+        if (!(data.caster() instanceof Player caster)) return new CastResult(PostCastAction.ALREADY_HANDLED, data);
 
-        String msg = "<rainbow>Hello World!</rainbow>";
-        Component text = MiniMessage.miniMessage().deserialize(msg);
-        Bukkit.getOnlinePlayers().forEach((player) -> {
-            Audience aud = (Audience) player;
-            aud.sendMessage(text);
-        });
+        String msg = "Hello World!";
+        caster.sendMessage(msg);
+
         playSpellEffects(data);
         return new CastResult(PostCastAction.ALREADY_HANDLED, data);
     }
